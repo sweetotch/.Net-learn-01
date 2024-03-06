bf82efb51ba42ee9171e71e5b33f49419c358c8b
mostapciuc <sweetotch@mail.ru>
2024-03-06 10:16:33 +0000

    Revert "[mo] - delete unsused references"
    This reverts commit 2285b30ef0ac735baafd94e277ed67d6d01c3f6c.

--- MO_Learn_CmdApp_01/Program.cs
+++ MO_Learn_CmdApp_01/Program.cs
@@ -1,6 +1,10 @@
 ﻿namespace MO_Learn_CmdApp_01
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
     class Calculator
     {
